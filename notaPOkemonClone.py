66d0903ac09cba05075b9c275e920419e29a6473
imakwana <59711042+imakwana@users.noreply.github.com>
2020-11-07 19:38:39 -0700

    Update notaPOkemonClone.py

--- notaPOkemonClone.py
+++ notaPOkemonClone.py
@@ -5,7 +5,8 @@ class player ():
         self.health = health
         self.speed = speed
         self.moves = moves 
-    
+        self.attack = 1
+
     def nameSet(self):
         name =""
         while name == "":
@@ -38,23 +39,37 @@ class player ():
         return self.moves[move] 
     def setMoves(self, moves):
         self.moves = moves
-
+    def setAttack(self, attack):
+        self.attack = attack
+    def getAttack(self):
+        return self.attack
 
     pass
-def battleSetUp():
-    choice = input("[1]Player vs Player          [2]Player vs Computer")
-    if choice == "1":
-        choice = input("")
-        player1 = playerSetUp("1")
+class ai(player):
+    def nameManualSet(self, name):
+        self.name = name
+    pass
+    
 
-def playerSetUp(number):
+def battleSetUp():
+        choice = input("")
+        player1 = playerSetUp()
+        AI = aiSetup()
+
+def playerSetUp():
     p = player()
     p._init_()
-    print("Player "+number +" Setup")
-    p.nameSet(input("What name should your character have."))
+    print("Player Setup")
+    p.nameSet()
     return p
+def aiSetup():
+    ai = ai()
+    names = ["Defrim", "Reynel", "Ishaan", "Francis"]
+    ai.nameManualSet(names[random.randrange(0, 3, 1)])
+    if ai.name == "Defrim":
+        ai.healthSet()
+    return ai
 
-<<<<<<< HEAD
 
 def Choose_Moves():
     Preset_Moves = {"Hyper Beam" : 150,
@@ -89,8 +104,10 @@ def Choose_Moves():
 if __name__ == "__main__":
     Choose_Moves()
     
-=======
-def battle():
+def battle(player1, player2):
 
+    if player1.getSpd() > player2.getSpd()
+        print("Player 1 is f"
+
+    pass
 #okay
->>>>>>> 04891875f2a778b0e75f5c9bcb3ecd3a8b4757c5
