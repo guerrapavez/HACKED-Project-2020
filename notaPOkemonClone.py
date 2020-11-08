d75942faa845716b3de575fb139e0bf27fcb8a7d
guerrapavez <55634251+guerrapavez@users.noreply.github.com>
2020-11-07 19:18:48 -0700

    moves

--- notaPOkemonClone.py
+++ notaPOkemonClone.py
@@ -36,9 +36,10 @@ class player ():
         self.speed = speed
     def MoveSelect(self,move):
         return self.moves[move] 
+    def setMoves(self, moves):
+        self.moves = moves
 
-if __name__ == "__main__":
-    
+
     pass
 def battleSetUp():
     choice = input("[1]Player vs Player          [2]Player vs Computer")
@@ -53,4 +54,37 @@ def playerSetUp():
     p.nameSet(input("What name should your character have."))
     return p
 
-#okay
+
+def Choose_Moves():
+    Preset_Moves = {"Hyper Beam" : 150,
+    "Fire Blast" : 110,
+    "Earthquake" : 100,
+    "Leaf Storm" : 130,
+    "Thunder Bolt" : 90,
+    "Close Combat" : 120,
+    "Psychic" : 90,
+    "Blizzard": 110,
+    "Bullet punch" : 40,
+    "Tackle" : 40,
+    "Hidden Power" : 60,
+    "Night Slash" : 70,
+    "Aerial Ace" : 60,
+    "WOAH WOAH WOAH" : 200
+    } 
+    p = player() 
+    p._init_()
+    
+    print(Preset_Moves)
+    Chosen_Moves = list(map(str,input("Please choose 2 moves from this list").split()))
+    player_chosen_moves = {}
+
+
+    for x in Chosen_Moves: 
+        if x in Preset_Moves:
+            player_chosen_moves[x] = Preset_Moves[x]
+    
+    p.setMoves(player_chosen_moves)
+    print(p.moves)
+if __name__ == "__main__":
+    Choose_Moves()
+    
